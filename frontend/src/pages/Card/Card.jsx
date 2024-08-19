d52a4ffbc0d6ec465ceaec6c33f805b24a068516
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-08-19 10:00:09 +0600

    cardItem completed

--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,23 +1,23 @@
 
-// import { Card } from "flowbite-react";
-// import { Link } from "react-router-dom";
+import { Card } from "flowbite-react";
+import { Link } from "react-router-dom";
 
-// const CardItem = ({title,description}) => {
-//     return (
-//         <div>
-//             <Link to={`details/${title}`}>
-//                 <Card className="max-w-sm bg-slate-200">
-//                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-//                        {title}
-//                     </h5>
-//                     <div style={{ height: '1px', border: '1px solid black' }} className="m-0"></div>
-//                     <p className="font-normal text-gray-700 dark:text-gray-400">
-//                         {description}
-//                     </p>
-//                 </Card>
-//             </Link>
-//         </div>
-//     );
-// };
+const CardItem = ({title,description}) => {
+    return (
+        <div>
+            <Link to={`details/${title}`}>
+                <Card className="max-w-sm bg-slate-200">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                       {title}
+                    </h5>
+                    <div style={{ height: '1px', border: '1px solid black' }} className="m-0"></div>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {description}
+                    </p>
+                </Card>
+            </Link>
+        </div>
+    );
+};
 
-// export default CardItem;
+export default CardItem;
